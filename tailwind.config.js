65c111c6ab4c32e8e4786bfd9062bb6c702db24f
Icaro Cristolfe <cristolfeicaro@gmail.com>
2022-12-05 13:15:35 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {  colors: {
+      'blue-facebook': '#1877F2',
+      'gray-facebook': '#F0F2F5',
+      'green-facebook': '#36A420'
+    },
+    spacing: {
+      '580': '580px',
+      '350': '350px',
+      '400': '400px'
+    }},
+  },
+  plugins: [],
+}
